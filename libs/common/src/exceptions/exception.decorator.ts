d63dd03c78f7ae8c095c29388aece5e308927b35
mustafaKamal-fe <gistcodee@gmail.com>
2023-07-17 18:31:54 +0300

    refactor: change system error

--- libs/common/src/exceptions/exception.decorator.ts
+++ libs/common/src/exceptions/exception.decorator.ts
@@ -1,10 +1,11 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiBadRequestResponse, getSchemaPath } from '@nestjs/swagger';
 import { CustomAppErrorDto, ValdiationErrorDto } from './exceptions.dto';
+
 export function apiExceptionResponse() {
   return applyDecorators(
     ApiBadRequestResponse({
-      description: 'Bad request',
+      description: 'System error',
       schema: {
         oneOf: [
           { $ref: getSchemaPath(CustomAppErrorDto) },
