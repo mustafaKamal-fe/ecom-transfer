122cc3d379c4165cf89a27ec64639f32b2280547
mustafaKamal-fe <gistcodee@gmail.com>
2023-07-17 18:31:39 +0300

    refactor: add exception dto classes to swagger module inside main file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppExceptionFilter } from '@common/common/exceptions/custom-exception.filter';
+import {
+  CustomAppErrorDto,
+  ValdiationErrorDto,
+} from '@common/common/exceptions/exceptions.dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
@@ -20,7 +24,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    extraModels: [ValdiationErrorDto, CustomAppErrorDto],
+  });
   SwaggerModule.setup('api', app, document);
 
   // Start app
